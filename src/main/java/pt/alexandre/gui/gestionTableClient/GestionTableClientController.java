0df0f706132ad60b3e3f2077c7067c993fb395f3
Alexandre <alexandre.lourencinho@gmail.com>
2021-07-25 22:13:44 +0200

    tentative DAO - a vérifier + Javadoc + transcodeur keyrelease + début gestion table client - crud

--- src/main/java/pt/alexandre/gui/gestionTableClient/GestionTableClientController.java
+++ src/main/java/pt/alexandre/gui/gestionTableClient/GestionTableClientController.java
@@ -3,6 +3,7 @@ package pt.alexandre.gui.gestionTableClient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -44,6 +45,7 @@ public class GestionTableClientController {
         colonnePrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
         masquerPaneDetails();
         listeDesClients();
+
     }
 
     public void afficherPaneDetails()
@@ -64,6 +66,26 @@ public class GestionTableClientController {
         tableauClient.setItems(model);
     }
 
+    public void modifierClient()
+    {
+        afficherPaneDetails();
+        txtNom.setText(tableauClient.getSelectionModel().getSelectedItem().getNom());
+        txtPrenom.setText(tableauClient.getSelectionModel().getSelectedItem().getPrenom());
+        txtVille.setText(tableauClient.getSelectionModel().getSelectedItem().getVille());
+    }
+
+    public void supprimerUnClient()
+    {
+        ClientsDAO reqCli = new ClientsDAO();
+        System.out.println(tableauClient.getSelectionModel().getSelectedIndex());
+        int id = tableauClient.getSelectionModel().getSelectedItem().getId()+1;
+        if(reqCli.supprimerClient(id)){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Client supprimé");
+            alert.setContentText("Vous avez bien supprimé le client de la base de données");
+            alert.showAndWait();
+        }
+    }
 
 
 }
